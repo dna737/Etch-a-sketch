004af5492779284abd09ede39007c04183c1b167
dna737 <adnanhere27@gmail.com>
2022-07-21 07:49:17 +0530

    be able to draw black strokes on white canvas

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ let pixelSize = 500/rowSize;
 
 //creating the grid:
 for(let i = 1; i <= rowSize ** 2 ;i++){
-    let div = document.createElement("div");
-    div.style.border = "1px dotted blue";
-    container.appendChild(div);
-    div.style.width = "" + pixelSize + "px";
-    div.style.height = "" + pixelSize + "px";
-
+    let pixel = document.createElement("div");
+    // pixel.style.border = "1px dotted blue";
+    container.appendChild(pixel);
+    pixel.style.width = "" + pixelSize + "px";
+    pixel.style.height = "" + pixelSize + "px";
+    pixel.style.margin = "0px";
+    pixel.style.border = "0px";
+    pixel.classList.add("pixel");
 }
+
+let pixels = document.querySelectorAll(".pixel");
+pixels.forEach(element => {
+    element.addEventListener('mouseover', () => {element.style.backgroundColor = "black"});
+});
+
     
